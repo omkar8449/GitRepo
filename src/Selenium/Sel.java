d4cc67cc946639e3225a1be865afc32690bee188
Omkar Gujar <ogujar108@gmail.com>
2023-01-16 16:18:32 +0530

    frst

---
+++ src/Selenium/Sel.java
@@ -0,0 +1,14 @@
+package Selenium;
+
+import org.openqa.selenium.chrome.ChromeDriverInfo;
+
+import AdvSelenium.ChromeDriver;
+import AdvSelenium.WebDriver;
+
+public class Sel {
+
+	public static void main(String[] args) {
+		WebDriver d=new ChromeDriver();
+	}
+
+}
